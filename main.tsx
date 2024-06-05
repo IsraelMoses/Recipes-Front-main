3410fdd71bdb58a99e2ac0e4f20812b7aa8905ad
IsraelMoses <135527634+IsraelMoses@users.noreply.github.com>
2024-06-05 14:06:19 +0300

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <GoogleOAuthProvider clientId="965008521931-eq4010ukchibqsc69orami7385pk5grm.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
+);
